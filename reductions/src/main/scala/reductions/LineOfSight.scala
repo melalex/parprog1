d0da3c650397f7bbd59bb3c1f540f4f43ecb429c
Alexander Melashchenko <melalex490@gmail.com>
2020-02-19 15:51:10 +0200

    Line of sight

--- reductions/src/main/scala/reductions/LineOfSight.scala
+++ reductions/src/main/scala/reductions/LineOfSight.scala
@@ -9,7 +9,7 @@ object LineOfSightRunner {
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 100,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val length = 10000000
@@ -41,49 +41,85 @@ case class Leaf(from: Int, until: Int, maxPrevious: Float) extends Tree
 object LineOfSight extends LineOfSightInterface {
 
   def lineOfSight(input: Array[Float], output: Array[Float]): Unit = {
-    ???
+
+    @scala.annotation.tailrec
+    def iter(i: Int, max: Float): Unit = if (i < input.length) {
+      val elem = Math.max(input(i) / i, max)
+      output(i) = elem
+      iter(i + 1, elem)
+    }
+
+    output(0) = 0
+    iter(1, 0)
   }
 
   /** Traverses the specified part of the array and returns the maximum angle.
    */
   def upsweepSequential(input: Array[Float], from: Int, until: Int): Float = {
-    ???
+    @scala.annotation.tailrec
+    def iter(i: Int, acc: Float): Float =
+      if (i < until) iter(i + 1, Math.max(input(i) / i, acc))
+      else acc
+
+    iter(from, 0)
   }
 
   /** Traverses the part of the array starting at `from` and until `end`, and
-   *  returns the reduction tree for that part of the array.
+   * returns the reduction tree for that part of the array.
    *
-   *  The reduction tree is a `Leaf` if the length of the specified part of the
-   *  array is smaller or equal to `threshold`, and a `Node` otherwise.
-   *  If the specified part of the array is longer than `threshold`, then the
-   *  work is divided and done recursively in parallel.
+   * The reduction tree is a `Leaf` if the length of the specified part of the
+   * array is smaller or equal to `threshold`, and a `Node` otherwise.
+   * If the specified part of the array is longer than `threshold`, then the
+   * work is divided and done recursively in parallel.
    */
   def upsweep(input: Array[Float], from: Int, end: Int,
-    threshold: Int): Tree = {
-    ???
+              threshold: Int): Tree = {
+    val length = end - from
+    if (length <= threshold) Leaf(from, end, upsweepSequential(input, from, end))
+    else {
+      val mid = from + length / 2
+
+      val (left, right) = parallel(upsweep(input, from, mid, threshold), upsweep(input, mid, end, threshold))
+
+      Node(left, right)
+    }
   }
 
   /** Traverses the part of the `input` array starting at `from` and until
-   *  `until`, and computes the maximum angle for each entry of the output array,
-   *  given the `startingAngle`.
+   * `until`, and computes the maximum angle for each entry of the output array,
+   * given the `startingAngle`.
    */
   def downsweepSequential(input: Array[Float], output: Array[Float],
-    startingAngle: Float, from: Int, until: Int): Unit = {
-    ???
+                          startingAngle: Float, from: Int, until: Int): Unit = {
+
+    @scala.annotation.tailrec
+    def iter(i: Int, max: Float): Unit = if (i < until) {
+      val elem = Math.max(input(i) / i, max)
+      output(i) = elem
+      iter(i + 1, elem)
+    }
+
+    iter(from, startingAngle)
   }
 
   /** Pushes the maximum angle in the prefix of the array to each leaf of the
-   *  reduction `tree` in parallel, and then calls `downsweepSequential` to write
-   *  the `output` angles.
+   * reduction `tree` in parallel, and then calls `downsweepSequential` to write
+   * the `output` angles.
    */
-  def downsweep(input: Array[Float], output: Array[Float], startingAngle: Float,
-    tree: Tree): Unit = {
-    ???
+  def downsweep(input: Array[Float], output: Array[Float], startingAngle: Float, tree: Tree): Unit = tree match {
+    case Leaf(from, to, _) => downsweepSequential(input, output, startingAngle, from, to)
+    case Node(left, right) => parallel(
+      downsweep(input, output, startingAngle, left),
+      downsweep(input, output, left.maxPrevious, right)
+    )
   }
 
   /** Compute the line-of-sight in parallel. */
-  def parLineOfSight(input: Array[Float], output: Array[Float],
-    threshold: Int): Unit = {
-    ???
+  def parLineOfSight(input: Array[Float], output: Array[Float], threshold: Int): Unit = {
+    val tree = upsweep(input, 1, input.length, threshold)
+
+    downsweep(input, output, 0, tree)
+
+    output(0) = 0
   }
 }
--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -57,15 +57,30 @@ object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterfac
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    @scala.annotation.tailrec
+    def traverse(i: Int, until: Int, acc: Int, unOpenedParentheses: Int): (Int, Int) = {
+      if (i < until) chars(i) match {
+        case '(' => traverse(i + 1, until, acc + 1, unOpenedParentheses)
+        case ')' if acc > 0 => traverse(i + 1, until, acc - 1, unOpenedParentheses)
+        case ')' => traverse(i + 1, until, acc, unOpenedParentheses + 1)
+        case _ => traverse(i + 1, until, acc, unOpenedParentheses)
+      }
+      else (acc, unOpenedParentheses)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) /*: ???*/ = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = from + (until - from) / 2
+
+        val ((acc1, unOpenedParentheses1), (acc2, unOpenedParentheses2)) = parallel(reduce(from, mid), reduce(mid, until))
+
+        if (acc1 > unOpenedParentheses2) (acc1 - unOpenedParentheses2 + acc2) -> unOpenedParentheses1
+        else acc2 -> (unOpenedParentheses2 - acc1 + unOpenedParentheses1)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
